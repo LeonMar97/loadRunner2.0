fe3d8dfb3198de7b68da4f9a6b560232be3a60cd
Leon Markovich <leonmarkovich97@gmail.com>
2021-01-02 13:14:50 +0200

    nothing speacial

--- include/Game_object.h
+++ include/Game_object.h
@@ -9,9 +9,10 @@ public:
     //-----------------------------functions------------------------------//
     Game_Object(sf::RectangleShape cur_rec ,char icon=' ') : m_Elemnt_Of_Game(cur_rec),
                                                                             m_Type(icon){};
-    virtual void draw_On_Board(sf::RenderWindow &m_Game_screen) ;
+   // virtual void draw_On_Board(sf::RenderWindow m_Game_screen) = 0;
    // virtual void effect() = 0;
 private:
      sf::RectangleShape m_Elemnt_Of_Game;
      char m_Type;//just to know which object is it
+
 };
