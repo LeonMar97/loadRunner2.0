beb29de4ef2ede7b2a6ccd111de5d74689e8b978
Leon Markovich <leonmarkovich97@gmail.com>
2021-01-08 00:35:49 +0200

    Removed leftovers from debugging.

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -98,7 +98,7 @@ while (i < sizeof_Map.y+1) {
 		cur_Rec.setOrigin(sf::Vector2f(cur_Rec.getGlobalBounds().width / 2, cur_Rec.getGlobalBounds().height / 2));
 		switch (m_Board.what_In_Location(sf::Vector2i(i, j))) {
 		case player:
-			//cur_Rec.setSize(player_Size);
+			cur_Rec.setSize(player_Size);
 			pic = new sf::Texture;
 			name = "player.png";
 			pic->loadFromFile(name);
