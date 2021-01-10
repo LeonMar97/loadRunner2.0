af82181b049f71fc1f6d3542d768a353d70e8e5f
adi00078 <76657987+adi00078@users.noreply.github.com>
2021-01-10 13:13:14 +0200

    fix move center of all staff6

--- src/Moving_Object.cpp
+++ src/Moving_Object.cpp
@@ -25,12 +25,14 @@ void Moving_Object::effect(void* key, std::vector<Game_Object*>m_All_Objects[NUM
 	case sf::Keyboard::Key::Down:
 		
 		 temp.move(0, m_Elemnt_Of_Game.getGlobalBounds().height * 0.1f);
-		 if (What_In_Loc(m_Elemnt_Of_Game, m_All_Objects) == pole) {
+		/* if (What_In_Loc(m_Elemnt_Of_Game, m_All_Objects) == pole) {
 			 m_Elemnt_Of_Game.move(0, m_Elemnt_Of_Game.getGlobalBounds().height*0.75);
 			 break;
 		 }
+		 */
+		 
 		 if (check_movment(temp, m_All_Objects, *this)) {
-			 m_Elemnt_Of_Game.move(0, m_Elemnt_Of_Game.getGlobalBounds().height * 0.1f);
+			 m_Elemnt_Of_Game.move(0,m_Elemnt_Of_Game.getGlobalBounds().height * 0.1f);
 			 m_Elemnt_Of_Game.setTexture(m_Tex[0]);
 		 }
 		break;
@@ -69,6 +71,7 @@ void Moving_Object::effect(void* key, std::vector<Game_Object*>m_All_Objects[NUM
 
 }
 //=============================================================================
+//check what in the next loc user pressed
 bool Moving_Object::check_movment(sf::RectangleShape& cur_Loc, std::vector<Game_Object*>m_All_Objects[],
 																				Game_Object& last_Loc){	
 	switch (What_In_Loc(cur_Loc, m_All_Objects)) {
@@ -88,16 +91,11 @@ bool Moving_Object::check_movment(sf::RectangleShape& cur_Loc, std::vector<Game_
 		
 		
 	};
-
-			
-
-
-		
-		
 	
 	return true;
 }
 //=========================================================================
+//check if next loc is ladder
 bool Moving_Object::check_ladder(sf::RectangleShape& cur_Loc, std::vector<Game_Object*>m_All_Objects[],
 																						Game_Object& last_Loc)
 {
@@ -105,8 +103,8 @@ bool Moving_Object::check_ladder(sf::RectangleShape& cur_Loc, std::vector<Game_O
 		char cur_Type = What_In_Loc(temp,m_All_Objects);
 	
 		sf::RectangleShape floor(m_Elemnt_Of_Game);
-		floor.move(0, 5.0f);
-		char under = What_In_Loc(floor, m_All_Objects);
+		floor.move(0, m_Elemnt_Of_Game.getGlobalBounds().height * 0.1f);
+		char under = What_In_Loc(floor, m_All_Objects);//check whar under me
 		switch (cur_Type) {
 
 			case  ladder:
@@ -170,7 +168,9 @@ void Moving_Object::on_Floor(std::vector<Game_Object*>m_All_Objects[])
 	under_Me.move(0, m_Elemnt_Of_Game.getGlobalBounds().height * 0.1f);//the floor under the current elemnt
 	char under_Me_Type = What_In_Loc(under_Me, m_All_Objects);
 	int key = (sf::Keyboard::Down);
-	if ((under_Me_Type != wall && under_Me_Type != ladder && under_Me_Type != pole)&&What_In_Loc(m_Elemnt_Of_Game, m_All_Objects)!=pole)
+	if (What_In_Loc(m_Elemnt_Of_Game, m_All_Objects) == pole)//if we are on pole dont drop
+		return;
+	if ((under_Me_Type != wall && under_Me_Type != ladder && under_Me_Type != pole))
 		this->effect(&key, m_All_Objects);
 	
 }
@@ -179,13 +179,13 @@ void Moving_Object::on_Floor(std::vector<Game_Object*>m_All_Objects[])
 bool Moving_Object::check_pole(std::vector<Game_Object*>m_All_Objects[],
 	Game_Object& last_Loc, sf::RectangleShape& cur_Loc)
 {
-	
+	/*
 	sf::RectangleShape temp(last_Loc.get_rectangle());
 	if (cur_Loc.getPosition().y - last_Loc.get_rectangle().getPosition().y <0.1f) {
 		m_Elemnt_Of_Game.setTexture(this->m_Tex[1]);
-		//m_Elemnt_Of_Game.move(0, 10.5f + m_Elemnt_Of_Game.getLocalBounds().height / 2);
+		m_Elemnt_Of_Game.move(0, 10.5f + m_Elemnt_Of_Game.getLocalBounds().height / 2);
 		//move_to_center(m_All_Objects);
-		return true;
+		return false;
 	}
 	if (What_In_Loc(temp, m_All_Objects) == pole) {
 
@@ -199,11 +199,23 @@ bool Moving_Object::check_pole(std::vector<Game_Object*>m_All_Objects[],
 		//move_to_center(m_All_Objects);
 		
 	}
-	m_Elemnt_Of_Game.move(0, 0.5f + m_Elemnt_Of_Game.getLocalBounds().height / 2);
-		return false;
+	*/
+	sf::RectangleShape temp(cur_Loc);
+	temp.move(0, 0.5f);
+	//if (cur_Loc.getPosition().y - last_Loc.get_rectangle().getPosition().y > 0.1f && What_In_Loc(cur_Loc, m_All_Objects) == space)
+	if (What_In_Loc(temp,m_All_Objects)==pole)
+	{
+		m_Elemnt_Of_Game.setTexture(this->m_Tex[1]);
+
+		m_Elemnt_Of_Game.move(0, m_Elemnt_Of_Game.getLocalBounds().height / 4);
+	}
+	if(What_In_Loc(cur_Loc,m_All_Objects)==space)
+	m_Elemnt_Of_Game.setTexture(this->m_Tex[1]);
+		return true;
 	
 }
 //-------------------------------------------------------------------------------
+//move object to the center of ladder or pole
 void Moving_Object::move_to_center(std::vector<Game_Object*>m_All_Objects[])
 {
 	for (int i = 0; i < NUM_OF_OBJECTS; i++)
