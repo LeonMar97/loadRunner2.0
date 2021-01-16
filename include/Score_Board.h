45b911f2e472fb22d0ee25867818d42acf684954
Leon Markovich <leonmarkovich97@gmail.com>
2021-01-16 19:46:34 +0200

    shit visual studio want

---
+++ include/Score_Board.h
@@ -0,0 +1,20 @@
+#include <cstdlib>
+#include <iostream>
+#include "Sounds_E.h"
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include "Textures.h"
+
+class Score_Board {
+	
+	//~~~~~~~~~~~~~~~~~~~~~~~functions~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+public:
+
+
+
+
+		//~~~~~~~~~~~~~~~~~~~~~~~members~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+private:
+
+
+}
---
+++ src/Score_Board.cpp
@@ -0,0 +1,1 @@
+#include "Score_Board.h"
